40f92ef568f4ac95ef754d7a33c033699e653968
Nerwen <jenny2209@gmx.de>
2022-06-20 11:34:03 +0200

    implement addBookToLibrary function

---
+++ main.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBooks() {}
