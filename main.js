0b7a834c2106bf979a85927fe2c475bf7d123e43
Nerwen <jenny2209@gmx.de>
2022-06-20 13:39:39 +0200

    implement displayBooks() function

--- main.js
+++ main.js
@@ -11,4 +11,25 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function displayBooks() {}
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    let author = document.createElement("p");
+    let title = document.createElement("p");
+    let pages = document.createElement("p");
+    let read = document.createElement("p");
+    author.textContent = `Author: ${book.author}`; // create dom element with text
+    title.textContent = `Title: ${book.title}`;
+    pages.textContent = `Pages: ${book.pages}`;
+    read.textContent = `Read: ${book.read}`;
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.append(author, title, pages, read);
+
+    const sect = document.querySelector("section");
+    sect.append(card);
+  });
+}
+
+const hobbit = new Book("J.R.R Tolkien", "The Hobbit", 320, true);
+const sofie = new Book("Jostein Gaarder", "Sofie's Welt", 618, false);
