c57d64bf92cc43c7f7e025c86230e9d854e52606
Nerwen <jenny2209@gmx.de>
2022-06-22 15:57:57 +0200

    add form pop up and implement add new book button functionality

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 let myLibrary = [];
+let formPopUp = document.querySelector(".form-pop-up");
+
+const addBookButton = document.getElementById("add-book-btn");
+const submitBookButton = document.getElementById("submit-book-btn");
+const cancelBookButton = document.getElementById("cancel-book-btn");
+
+addBookButton.addEventListener("click", openForm);
+cancelBookButton.addEventListener("click", closeForm);
+submitBookButton.addEventListener("click", () => {
+  const book = readFormData();
+  addBookToLibrary(book);
+  closeForm();
+  displayBooks();
+});
+
+function openForm() {
+  formPopUp.style.display = "block";
+}
+
+function closeForm() {
+  formPopUp.style.display = "none";
+}
+
+function readFormData() {
+  const author = formPopUp.elements["author"].value;
+  const title = formPopUp.elements["title"].value;
+  const pages = formPopUp.elements["pages"].value;
+  const read = formPopUp.elements["read"].checked;
+  return new Book(author, title, pages, read);
+}
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -29,7 +59,11 @@ function displayBooks() {
     const sect = document.querySelector("section");
     sect.append(card);
   });
+  myLibrary = [];
 }
 
+// Just for testing purpose TODO: Remove after fully implemented
 const hobbit = new Book("J.R.R Tolkien", "The Hobbit", 320, true);
 const sofie = new Book("Jostein Gaarder", "Sofie's Welt", 618, false);
+addBookToLibrary(hobbit);
+addBookToLibrary(sofie);
